be694a62aa871fb8e3b0d50730b176eab5c3b035
Hassan Koraim <hassankoraim2@gmail.com>
2024-07-02 00:57:37 +0200

    intial commit: Data-structures/Queue

---
+++ Data-structures/Queue/Queue/AQueue.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+/*
+ * This implementation Queue using Arrays
+ * we use Array of integer data type 
+ */
+
+namespace Queue
+{
+    public class AQueue
+    {
+        private int[] queue;
+        private int front, rear, capacity;
+
+        // Constructor
+        public AQueue(int size)
+        {
+            queue = new int[size];
+            capacity = size;
+            front = rear = 0;
+        }
+        public bool isEmpty()
+        {
+            return front == rear;  // front and rear is equal 0 
+        }
+        public bool isFull()
+        {
+            return capacity == rear; 
+        }
+        public void Enqueue(int item) {
+            if (isFull()) 
+            {
+                Console.WriteLine("this array is full"); //overflow condition
+            }
+            else
+            {
+                queue[rear] = item;
+                rear++;
+            }
+            
+        }
+        public void Dequeue()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("This array is Empty"); // underflow Condition
+                return;
+            }
+            else
+            {
+                // shifting the element to the lift side
+                for(int i = 0; i < rear -1; i++) {
+                    queue[i] = queue[i +1];        
+                }
+                //decrement rear
+                rear--;
+                // delete the element by put the value to = 0 
+                queue[rear] = 0;
+            }
+        }
+        public int Peek()
+        {
+            return queue[front];
+        }
+        public void display()
+        {
+            if(isEmpty())
+            {
+                Console.WriteLine("didn't find any item to display!!");
+               return;
+            }
+            else
+            {
+                for (int i = 0; i < rear; i++)
+                {
+                    Console.WriteLine(queue[i]);
+                }
+                return;
+            }
+            
+        }
+       
+
+    }
+}
---
+++ Data-structures/Queue/Queue/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AQueue queue = new AQueue(5);
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue(); queue.Dequeue();
+            queue.Dequeue();
+            queue.display();
+
+        }
+    }
+}
