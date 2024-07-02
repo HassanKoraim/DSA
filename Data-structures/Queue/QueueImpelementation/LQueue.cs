613ac2e9e951cf822d5e98ee738db5bb45b8d6c4
Hassan Koraim <hassankoraim2@gmail.com>
2024-07-03 00:36:18 +0200

    The Impelementation of Queue by LinkedList

---
+++ Data-structures/Queue/QueueImpelementation/LQueue.cs
@@ -0,0 +1,70 @@
+﻿using LinkedListImplementaionExamples;
+using QueueImplemention;
+using System;
+
+namespace QueueImplementation
+{
+    public class LQueue : Queue
+    {
+        Node<int>? head,  curr, tail;
+        public LQueue()
+        {
+             head = curr = tail = new Node<int>(null);
+        }
+
+        public bool isEmpty()
+        {
+
+            return head == null;
+        }
+
+        public bool isFull()
+        {
+            return false; // the linkedlist is dynamic memory
+        }
+
+        public void Enqueue(int item)
+        {
+            Node<int>? newNode = new Node<int>(item, null);
+            if(isEmpty()) 
+            {
+                head = curr = tail = newNode;
+            }
+            else
+            {
+                
+                tail?.setNext(newNode);
+                tail = newNode;
+            }
+        }
+
+        public void Dequeue()
+        {
+            if(!isEmpty())
+            {
+                head = head?.Next();
+            }
+            else
+            {
+                Console.WriteLine("This Queue is Empty");
+                return;
+            }
+        }
+
+        public int Peek()
+        {
+            if (!isEmpty())
+                return head.getValue();
+            else return -1;
+        }
+        public void display()
+        {
+            curr = head?.Next();
+            while(curr != null) 
+            {
+                Console.WriteLine(curr.getValue());
+                curr = curr.Next();
+            }
+        }
+    }
+}
--- Data-structures/Queue/QueueImpelementation/Program.cs
+++ Data-structures/Queue/QueueImpelementation/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace QueueImplemention
+﻿using QueueImplementation;
+
+namespace QueueImplemention
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
+            LQueue queue = new LQueue();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            //Console.WriteLine(queue.Peek());
+          //  queue.Dequeue();
+          //  Console.WriteLine(queue.Peek());
+          //  queue.Dequeue();
+          //  Console.WriteLine(queue.Peek());
+        //    queue.Dequeue();
+            queue.display();
              
 
             #region Test For Queue implementation using Array
