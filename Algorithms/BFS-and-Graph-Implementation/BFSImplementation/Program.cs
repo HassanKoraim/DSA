6dbbdf1a3571f9cf608b1b93e343b082ada8a46c
Hassan Koraim <hassankoraim2@gmail.com>
2024-07-13 19:58:55 +0200

    Implement The BFS And Graph

---
+++ Algorithms/BFS-and-Graph-Implementation/BFSImplementation/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace BFSImplementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Number of vertices in the graph
+            int vertices = 5;
+
+            // Create a graph
+            Graph graph = new Graph(vertices);
+
+            // Add edges to the graph
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(1, 3);
+            graph.AddEdge(1, 4);
+            graph.AddEdge(2, 4);
+
+            // Perform BFS traversal starting from vertex 0
+            Console.Write(
+                "Breadth First Traversal starting from vertex 0: ");
+            graph.BFS(0);
+        }
+    }
+}
