f8574078d699b460ced0349854a079bac58b233c
Hassan Koraim <hassankoraim2@gmail.com>
2024-07-28 01:35:18 +0200

    Add Prblem SameTree

---
+++ Algorithms/ProblemSolving/DepthFirstSearch-Problems/SameTree/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SameTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(3);
+            root.left.left = new TreeNode(4);
+            root.left.right = new TreeNode(5);
+
+            TreeNode root2 = new TreeNode(1);
+            root2.left = new TreeNode(2);
+            root2.right = new TreeNode(3);
+            root2.left.left = new TreeNode(4);
+            root2.left.right = new TreeNode(5);
+            Solution s = new Solution();
+            Console.WriteLine(s.IsSameTree(root, root2));
+
+        }
+    }
+}
---
+++ Algorithms/ProblemSolving/DepthFirstSearch-Problems/SameTree/README.md
@@ -0,0 +1,1 @@
+# 100. Same Tree \n ## The link \n https://leetcode.com/problems/same-tree/description
