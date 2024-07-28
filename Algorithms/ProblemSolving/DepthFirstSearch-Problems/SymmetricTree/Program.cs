98b733b392ac1a979d1dc5c332b6de3409c9184f
Hassan Koraim <hassankoraim2@gmail.com>
2024-07-28 17:59:51 +0200

    Add SymmetricTree Problem

---
+++ Algorithms/ProblemSolving/DepthFirstSearch-Problems/SymmetricTree/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SymmetricTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(2);
+            root.left.left = new TreeNode(3);
+            root.right.right = new TreeNode(3);
+            root.left.right = new TreeNode(3);
+            root.right.left = new TreeNode(4);
+            Console.WriteLine(s.IsSymmetric(root));
+        }
+    }
+
+}
---
+++ Algorithms/ProblemSolving/DepthFirstSearch-Problems/SymmetricTree/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace SymmetricTree
+{
+    public class Solution
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if(root == null) return true;
+            return ISmirror(root.left, root.right);
+         
+        }
+        public bool ISmirror(TreeNode rootLeft, TreeNode rootRight)
+        {
+            if(rootLeft == null && rootRight == null) return true;
+            if(rootLeft == null || rootRight == null) return false;
+            if (rootLeft.val != rootRight.val) return false;
+            return (ISmirror(rootLeft.right, rootRight.left) && ISmirror(rootLeft.left , rootRight.right));
+        }
+    }
+
+}
