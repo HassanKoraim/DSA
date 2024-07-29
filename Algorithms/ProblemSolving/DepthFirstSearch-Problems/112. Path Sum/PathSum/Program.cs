4c2fb284970fa3f99e1fcac7aa6be37e3dcc0a48
Hassan Koraim <hassankoraim2@gmail.com>
2024-07-30 01:07:50 +0200

    add 112. Path Sum on ProblemSolving/DFS

---
+++ Algorithms/ProblemSolving/DepthFirstSearch-Problems/112. Path Sum/PathSum/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PathSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Algorithms/ProblemSolving/DepthFirstSearch-Problems/112. Path Sum/PathSum/Solution.cs
@@ -0,0 +1,12 @@
+﻿namespace PathSum
+{
+    public class Solution
+    {
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if(root == null) return false;
+            if(root.left == null &&  root.right == null) return root.val - targetSum == 0;
+            return HasPathSum(root.left, targetSum - root.val) || HasPathSum(root.right, targetSum - root.val);
+        }
+    }
+}
