e3ce30cd1d22bedd683824ee501dbe37d6c05f37
Hassan Koraim <hassankoraim2@gmail.com>
2024-08-03 14:34:17 +0200

    Add 110. Balanced Binary Tree Problem

---
+++ Algorithms/ProblemSolving/DepthFirstSearch-Problems/110. Balanced Binary Tree/Balanced_Binary_Tree/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Balanced_Binary_Tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(3);
+            root.left = new TreeNode(9);
+            root.right = new TreeNode(20);
+            root.right.left = new TreeNode(15);
+            root.right.right = new TreeNode(7);
+            Solution solution = new Solution();
+            Console.WriteLine(solution.IsBalanced(root));
+            Console.WriteLine(solution.checkLevel(root));
+        }
+    }
+}
---
+++ Algorithms/ProblemSolving/DepthFirstSearch-Problems/110. Balanced Binary Tree/Balanced_Binary_Tree/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace Balanced_Binary_Tree
+{
+    public class Solution
+    {
+        public bool IsBalanced(TreeNode root)
+        {
+            return CheckHeight(root) != -1;   //  if balanced or not
+        }
+        private int CheckHeight(TreeNode root)
+        {
+            if (root == null) return 0;
+            int leftHeight = CheckHeight(root.left);
+            if (leftHeight == -1) return -1;
+            int rightHeight = CheckHeight(root.right);
+            if (rightHeight == -1)return -1;
+            if (Math.Abs(leftHeight - rightHeight) > 1) return -1;  // not balanced
+            return Math.Max(leftHeight, rightHeight) + 1;
+        }
+
+       
+    }
+}
