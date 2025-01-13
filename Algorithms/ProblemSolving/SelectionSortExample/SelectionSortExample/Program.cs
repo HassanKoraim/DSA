423d67c98f2cf174886aca3fae6aa7f3fe9eb14e
Hassan Koraim <hassankoraim2@gmail.com>
2025-01-13 17:42:13 +0200

    Add SelectionSortExample Problem

---
+++ Algorithms/ProblemSolving/SelectionSortExample/SelectionSortExample/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+
+namespace SelectionSortExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size");
+            int size = Convert.ToInt32(Console.ReadLine());
+            List<int> inputs = new List<int>();
+            for (int i = 0; i < size; i++)
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                inputs.Add(num);
+            }
+            List<int> inputs_sorted = selection_sort(inputs, size);
+            // print the list
+            foreach (int num in inputs_sorted)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+        public static List<int> selection_sort(List<int> inputs, int size)
+        {
+            for (int i = 0; i < size - 1; i++)
+            {
+                int min_pos = i;
+                for (int j = i +1; j < size; j++)
+                {
+                    if (inputs[j] < inputs[min_pos])
+                    {
+                       min_pos = j;
+                    }
+                }
+                if (min_pos != i)
+                {
+                    int temp = inputs[i];
+                    inputs[i] = inputs[min_pos];
+                    inputs[min_pos] = temp;
+                }
+            }
+            return inputs;
+        }
+    }
+}
