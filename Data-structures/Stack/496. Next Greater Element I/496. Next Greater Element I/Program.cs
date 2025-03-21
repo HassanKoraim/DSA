a56f9af37a5d6dfd53ef7557040e9abfe308d28a
Hassan Koraim <hassankoraim2@gmail.com>
2025-03-21 14:34:24 +0200

    Add '496. Next Greater Element I' Problem

---
+++ Data-structures/Stack/496. Next Greater Element I/496. Next Greater Element I/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _496._Next_Greater_Element_I
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums1 = new int[3] { 4, 1, 2 };
+            int[] nums2 = new int[4] { 1, 3,4, 2 };
+            Solution solution = new Solution();
+            solution.NextGreaterElement(nums1, nums2);
+        }
+    }
+}
