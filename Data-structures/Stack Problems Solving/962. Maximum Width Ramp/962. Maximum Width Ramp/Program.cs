d7527451e8db601b979cf523b6e74f9aadfc2237
Hassan Koraim <hassankoraim2@gmail.com>
2025-04-07 00:30:14 +0200

    add '962. Maximum Width Ramp' Problem

---
+++ Data-structures/Stack Problems Solving/962. Maximum Width Ramp/962. Maximum Width Ramp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _962._Maximum_Width_Ramp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+   
+            Console.WriteLine(s.MaxWidthRamp(new int[] { 6, 0, 8, 2, 1, 5 }));
+        }
+    }
+}
---
+++ Data-structures/Stack Problems Solving/962. Maximum Width Ramp/962. Maximum Width Ramp/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _962._Maximum_Width_Ramp
+{
+    public class Solution
+    {
+        public int MaxWidthRamp(int[] nums)
+        {
+            int n = nums.Length;
+            int maxWidth = 0;
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                if (stack.Count == 0 || nums[i] < nums[stack.Peek()])
+                {
+                    stack.Push(i);
+                }
+            }
+            for (int j = n - 1; j >= 0; j--)
+            {
+                while (stack.Count > 0 && nums[j] >= nums[stack.Peek()])
+                {
+                    maxWidth = Math.Max(maxWidth, j - stack.Pop());
+                }
+            }
+            return maxWidth;
+        }
+
+    }
+}
